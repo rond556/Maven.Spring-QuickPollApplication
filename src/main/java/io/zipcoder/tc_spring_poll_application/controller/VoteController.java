49bed6984f685115f2a4b7d6adfda4f99fcb8ca4
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-12-07 14:25:27 -0500

    pushing

---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controller/VoteController.java
@@ -0,0 +1,37 @@
+package io.zipcoder.tc_spring_poll_application.controller;
+
+import io.zipcoder.tc_spring_poll_application.domain.Vote;
+import io.zipcoder.tc_spring_poll_application.repositories.VoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+public class VoteController {
+
+    private VoteRepository voteRepository;
+
+    @Autowired
+    public VoteController(VoteRepository voteRepository) {
+        this.voteRepository = voteRepository;
+    }
+
+    @PostMapping("/polls/{pollId}/votes")
+    public ResponseEntity<?> createVote(@PathVariable Long pollId, @RequestBody Vote
+            vote) {
+        vote = voteRepository.save(vote);
+        // Set the headers for the newly created resource
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setLocation(ServletUriComponentsBuilder.
+                fromCurrentRequest().path("/{id}").buildAndExpand(vote.getId()).toUri());
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+    }
+}
